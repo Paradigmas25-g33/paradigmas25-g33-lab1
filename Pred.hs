b3ad7d16624d3fd656191a67a74aaec59d021b86
Teby <tebygranja@gmail.com>
2025-04-02 15:43:06 -0300

    funciones arregladas para tomar subdibujos

--- Pred.hs
+++ Pred.hs
@@ -41,34 +41,36 @@ allDib p = foldDib (\x -> p x) id id id (\_ _ b1 b2 -> b1 && b2) (\_ _ b1 b2 -> 
 --Pred> allDib (even) (Encimar (Basica 8) (Basica 2))
 --True
 
--- Hay 4 rotaciones seguidas.
+-- Hay 4 rotaciones seguidas
 esRot360 :: Pred (Dibujo a)
 esRot360 dib = 
-    case foldDib
-        (\_ -> (0, False))  -- Caso base: figura básica
-        (\(n, _) -> (n + 1, n + 1 == 4))  -- Rotar: incrementa contador y verifica si llega a 4
-        (\(n, _) -> (0, False))  -- Rotar45: reinicia contador
-        (\(n, _) -> (0, False))  -- Espejar: reinicia contador
-        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Apilar: combina resultados
-        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Juntar: combina resultados
-        (\(n1, b1) (n2, b2) -> (0, b1 || b2))  -- Encimar: combina resultados
+    let check (n, found) = (n, found || n >= 4)
+    in case foldDib
+        (\_ -> (0, False))  -- Caso base
+        (\(n, found) -> check (n + 1, found))  -- Rotar
+        (\_ -> (0, False))  -- Rotar45
+        (\_ -> (0, False))  -- Espejar
+        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Apilar
+        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Juntar
+        (\(n1, b1) (n2, b2) -> (0, b1 || b2))  -- Encimar
         dib of
-    (_, True) -> True  -- Si se encontró una secuencia de 4 rotaciones
+    (_, True) -> True  
     _ -> False
 
--- Hay 2 espejados seguidos.
+-- Hay 2 espejados seguidos
 esFlip2 :: Pred (Dibujo a)
 esFlip2 dib = 
-    case foldDib
-        (\_ -> (0, False))  -- Caso base: figura básica
-        (\(n, _) -> (0, False))  -- Rotar: incrementa contador y verifica si llega a 4
-        (\(n, _) -> (0, False))  -- Rotar45: reinicia contador
-        (\(n, _) -> (n + 1, n + 1 >=2 ))  -- Espejar: reinicia contador
-        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Apilar: combina resultados
-        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Juntar: combina resultados
-        (\(n1, b1) (n2, b2) -> (0, b1 || b2))  -- Encimar: combina resultados
+    let check (n, found) = (n, found || n >= 2)
+    in case foldDib
+        (\_ -> (0, False))  -- Caso base
+        (\(n, b) -> (0, b))  -- Rotar
+        (\(n, b) -> (0, b))  -- Rotar45
+        (\(n, found) -> check (n + 1, found))  -- Espejar: incrementa contador y verifica si llega a 2
+        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Apilar
+        (\_ _ (n1, b1) (n2, b2) -> (0, b1 || b2))  -- Juntar
+        (\(n1, b1) (n2, b2) -> (0, b1 || b2))  -- Encimar
         dib of
-    (_, True) -> True  -- Si se encontró una secuencia de 4 rotaciones
+    (_, True) -> True 
     _ -> False
 
 
