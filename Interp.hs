48175169a11941029200a32c9a98d88a671a0295
Ramiro Alvarez <ramiroalvarez3217@mi.unc.edu.ar>
2025-04-09 10:30:59 -0300

    acercandonos a escher - Interp

--- Interp.hs
+++ Interp.hs
@@ -25,33 +25,34 @@ sub (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 -- Interpreta el operador de rotación
 interp_rotar :: ImagenFlotante -> ImagenFlotante --funciona
 interp_rotar f v1 v2 v3 =
-    let rotacion = (f (add v1 v2) v3 (sub (sub v2 v2) v2))
+    let rotacion = (f (add v1 v2) v3 (sub (0,0) v2))
     in rotacion
 
 -- Interpreta el operador de espejar
-interp_espejar :: ImagenFlotante -> ImagenFlotante --no funciona
+interp_espejar :: ImagenFlotante -> ImagenFlotante --funciona
 interp_espejar f v1 v2 v3 =
-    let espejo = f (add v1 v2) (sub (sub v2 v2) v2) v3
-    in espejo
+    let espejar = (f (add v1 (mitad (add v2 v3))) (mitad (add v2 v3)) (mitad (sub v3 v2)))
+    in espejar
+    
 
 -- interpreta el operador de rotación 45
-interp_rotar45 :: ImagenFlotante -> ImagenFlotante -- no funciona
+interp_rotar45 :: ImagenFlotante -> ImagenFlotante --funciona
 interp_rotar45 f v1 v2 v3 =
-    let rotacion = (f (add v1 (mitad (add v2 v3))) (mitad (add v2 v3)) (mitad (sub v3 v2)))
-    in rotacion
+    let rotar45 = f (add v1 v2) (sub (0,0) v2) v3
+    in rotar45
 
 -- interpreta el operador de apilar
 interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante --funciona
-interp_apilar n m f g v1 v2 v3 =
+interp_apilar m n f g v1 v2 v3 =
     let n' = fromIntegral n
         m' = fromIntegral m
-        apiladoF = f (add v1 ((n' / (m' + n')) V.* v3)) v2 ((n' / (m' + n')) V.* v3)
-        apiladoG = g v1 v2 ((m' / (m' + n')) V.* v3)
+        apiladoF = f (add v1 ((n' / (m' + n')) V.* v3)) v2 ((m' / (m' + n')) V.* v3)
+        apiladoG = g v1 v2 ((n' / (m' + n')) V.* v3)
     in pictures [apiladoF, apiladoG]
 
 -- interpreta el operador de juntar
 interp_juntar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante --funciona
-interp_juntar n m f g v1 v2 v3 =
+interp_juntar m n f g v1 v2 v3 =
     let m' = fromIntegral m
         n' = fromIntegral n
         juntadoF = f v1 ((m' / (m' + n')) V.* v2) v3
